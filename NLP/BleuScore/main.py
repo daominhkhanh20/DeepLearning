f5bbec2c6fee5cd5c056df363ca090c7812e8e1b
daominhkhanh20 <khanhc1k36@gmail.com>
2021-02-21 11:16:08 +0700

    add smooth values

--- NLP/BleuScore/main.py
+++ NLP/BleuScore/main.py
@@ -1,14 +1,15 @@
 from nltk import ngrams 
 from collections import Counter 
 import numpy as np 
+import nltk.translate .bleu_score as bleu
+import math 
+# translation = 'It is a guide to action which ensures that the military always obeys the commands of the party.'
+# references=[
+#     'It is a guide to action that ensures that the military will forever heed Party commands.',
+#     'It is the guiding principle which guarantees the military forces always being under the command of the Party.',
+#     'It is the practical guide for the army always to heed the directions of the party.'
+# ]
 
-
-translation = 'It is a guide to action which ensures that the military always obeys the commands of the party.'
-references=[
-    'It is a guide to action that ensures that the military will forever heed Party commands.',
-    'It is the guiding principle which guarantees the military forces always being under the command of the Party.',
-    'It is the practical guide for the army always to heed the directions of the party.'
-]
 
 def count_gram(sentences,ngram=1):
     return Counter(ngrams(sentences,ngram))
@@ -19,20 +20,17 @@ def count_clip_gram(result_translation,list_of_references,ngram=1):
 
     for sentence in list_of_references:
         sentence_gram=count_gram(sentence.split(),ngram=ngram)
-        for i in sentence_gram:
-            if i in result:
-                result[i]=max(result[i],sentence_gram[i])
-            else:
-                result[i]=sentence_gram[i]
-            
+        for i in translation_u:
+            result[i]=max(result.get(i,0),sentence_gram[i])
+
     return {
-        k: min(result.get(k,0),translation_u.get(k,0)) for k in translation_u
+        words:min(count,result[words]) for words,count in translation_u.items()
     }
  
 def modified_precision(result_translation,list_references,ngram=1):
-    count=count_gram(result_translation)
+    count=count_gram(result_translation,ngram=ngram)
     count_clip=count_clip_gram(result_translation,list_references,ngram=ngram)
-    return 1.0*sum(count_clip.values())/max(sum(count.values()),1)
+    return max(1.0*sum(count_clip.values())/max(sum(count.values()),1),1e-10)#smooth values
 
 def get_closest_reference(result_translation,list_of_references):
     len_trans=len(result_translation)
@@ -49,19 +47,30 @@ def brevity_penalty(result_translation,list_of_references):
 def final_bleu_score(result_translation,list_of_references):
     bp=brevity_penalty(result_translation,list_of_references)
     modified_precision_score=[
-        modified_precision(result_translation,list_of_references,ngram=i) \
-             for i in range(1,4,1) 
+        modified_precision(result_translation,list_of_references,ngram=i) for i in range(1,5,1) 
     ]
+  
     score=np.sum(
         [
-            0.25*np.log(modified_precision_score[i]) if modified_precision_score[i]>0 else 0 
-            for i in range(len(modified_precision_score))
+            0.25*math.log(modified_precision_score[i]) for i in range(len(modified_precision_score))
         ]
 
     )
+
     return bp*np.exp(score)
 
-bleu_score_modified_precision=modified_precision(translation.split(),references)
-print("BLEU Score Modified Precision :",bleu_score_modified_precision)
-bleu_final=final_bleu_score(translation.split(),references)
-print("BLEU Score Final :",bleu_final)
+if __name__=="__main__":
+    references=['The cat is on the mat.',
+            'There is a cat on the mat.'
+        ]
+
+    translation_1='the the the mat on the the.'
+    translation_2='The cat is on the mat.'
+    bleu_score_modified_precision=modified_precision(translation_1.split(),references)
+    print("BLEU Score Modified Precision :",bleu_score_modified_precision)
+    bleu_final=final_bleu_score(translation_2.split(),references)
+    print("BLEU Score Final :",bleu_final)
+
+    #using library
+    references_translation=[reference.split() for reference in references]
+    print("Bleu score:",bleu.sentence_bleu(references_translation,translation_2.split()))
